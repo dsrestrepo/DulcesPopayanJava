716ec1627110606aee66ffecfdeeaba29ef882e4
Camilo <mcaburbano@unicauca.edu.co>
2021-04-24 22:16:56 -0500

    rutas

--- app/src/main/java/edu/unicauca/appsmoviles/dulces_popayan_java2/ui/home/HomeViewModel.java
+++ app/src/main/java/edu/unicauca/appsmoviles/dulces_popayan_java2/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Disfruta de una dulce guía conociendo Popayán");
+        mText.setValue("");
     }
 
     public LiveData<String> getText() {
